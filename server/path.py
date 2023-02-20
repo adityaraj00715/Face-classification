44ec9742f3c5dba9b2ff0a95e4549987c83d4607
adityaraj00715 <adityaraj0071506@gmail.com>
2023-02-20 20:16:20 +0530

    adding server file

---
+++ server/path.py
@@ -0,0 +1,4 @@
+import os
+print('Get current working directory : ', os.getcwd())
+print(os.listdir('C:\SportsPersonClassifier\server\abc'))
+# print(os.path.exists('C:\SportsPersonClassifier\server\facts'))
