a09cd0be94cd20a54e43e4488e6809e7f5400d3c
adityaraj00715 <adityaraj0071506@gmail.com>
2023-02-20 20:19:44 +0530

    adding UI files

---
+++ UI/app.js
@@ -0,0 +1,79 @@
+Dropzone.autoDiscover = false;
+
+function init() {
+    let dz = new Dropzone("#dropzone", {
+        url: "/",
+        maxFiles: 1,
+        addRemoveLinks: true,
+        dictDefaultMessage: "Some Message",
+        autoProcessQueue: false
+    });
+    
+    dz.on("addedfile", function() {
+        if (dz.files[1]!=null) {
+            dz.removeFile(dz.files[0]);        
+        }
+    });
+
+    dz.on("complete", function (file) {
+        let imageData = file.dataURL;
+        
+        var url = "http://127.0.0.1:5000/classify_image";
+
+        $.post(url, {
+            image_data: file.dataURL
+        },function(data, status) {
+            /*
+            [{'class': 'virat kohli', 
+            'class_probability': [0.11, 26.73, 28.54, 0.0, 0.07, 44.55], 
+            'class_dictionary': 
+            {'ABD': 0, 'Leone_Messi': 1, 'MS_Dhoni': 2, 'Ronaldo': 3, 'SKY': 4, 'virat_kohli': 5}}]
+            */
+            console.log(data);
+            if (!data || data.length==0) {
+                $("#resultHolder").hide();
+                $("#divClassTable").hide();                
+                $("#error").show();
+                return;
+            }
+            let players = ["ABD", "Leone_Messi", "MS_Dhoni", "Ronaldo", "SKY", "virat_kohli"];
+            
+            let match = null;
+            let bestScore = -1;
+            for (let i=0;i<data.length;++i) {
+                let maxScoreForThisClass = Math.max(...data[i].class_probability);
+                if(maxScoreForThisClass>bestScore) {
+                    match = data[i];
+                    bestScore = maxScoreForThisClass;
+                }
+            }
+            if (match) {
+                $("#error").hide();
+                $("#resultHolder").show();
+                $("#divClassTable").show();
+                $("#resultHolder").html($(`[data-player="${match.class}"`).html());
+                let classDictionary = match.class_dictionary;
+                for(let personName in classDictionary) {
+                    let index = classDictionary[personName];
+                    let proabilityScore = match.class_probability[index];
+                    let elementName = "#score_" + personName;
+                    $(elementName).html(proabilityScore);
+                }
+            }
+            // dz.removeFile(file);            
+        });
+    });
+
+    $("#submitBtn").on('click', function (e) {
+        dz.processQueue();		
+    });
+}
+
+$(document).ready(function() {
+    console.log( "ready!" );
+    $("#error").hide();
+    $("#resultHolder").hide();
+    $("#divClassTable").hide();
+
+    init();
+});
